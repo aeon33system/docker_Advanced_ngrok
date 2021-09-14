310344aece5f6445bfdcdb23138fc667bdae65f1
aeon33system <aeon33system@gmail.com>
2021-09-14 22:20:40 +0800

    delete old lab4

--- lab4/check_url.sh
+++
@@ -1,3 +0,0 @@
-#!bin/bash
-curl $(docker port chatbot_ngrok 4040)/api/tunnels > tunnels.json
-docker run -v $(pwd)/tunnels.json:/tmp/tunnels.json --rm  realguess/jq jq .tunnels[1].public_url /tmp/tunnels.json 
